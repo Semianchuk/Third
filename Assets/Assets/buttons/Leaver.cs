3e5a74b325c8bae9b2425effb9171eecb5874b07
Semianchuk <senata155@gmail.com>
2022-04-16 10:50:51 +0300

    изменено
    что то

---
+++ Assets/Assets/buttons/Leaver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.buttons
+{
+    class Leaver : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _degreeTrue, _degreeFalse;
+        [SerializeField] private GameObject _point;
+        private Dictionary<bool, Vector3> dc = new Dictionary<bool, Vector3>();
+        private bool _isActive = false;
+
+        private void Awake()
+        {
+            dc.Add(true, _degreeTrue);
+            dc.Add(false, _degreeFalse);
+        }
+
+        private void Start()
+        {
+            _point.transform.eulerAngles = dc[_isActive];
+        }
+
+        private void OnMouseDown()
+        {
+            _isActive = !_isActive;
+            _point.transform.eulerAngles = dc[_isActive];
+        }
+    }
+}
---
+++ Assets/Assets/buttons/Thumbler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.buttons
+{
+    class Thumbler : MonoBehaviour
+    {
+        [SerializeField] private int[] _degrees;
+        private int _currentIndex = 0;
+
+        private void Start()
+        {
+            transform.rotation = Quaternion.Euler(0, 0, _degrees[0]);
+        }
+
+        private void OnMouseDown()
+        {
+            _currentIndex++;
+            _currentIndex %= _degrees.Length;
+
+            transform.rotation = Quaternion.Euler(0, 0, _degrees[_currentIndex]);
+        }
+    }
+}
